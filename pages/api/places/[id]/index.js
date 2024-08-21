e33dba1384157e01410317019ea833134d88d01e
Sercan Sebil <sercansebil95@googlemail.com>
2024-08-21 10:56:47 +0200

    addPlace function

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -6,7 +6,23 @@ export default async function handler(request, response) {
   const { id } = request.query;
 
   if (request.method === "GET") {
-    const place = await Place.findById(id);
-    response.status(200).json(place);
+    if (!id) {
+      return response
+        .status(400)
+        .json({ message: "Bad Request: ID is required" });
+    }
+
+    try {
+      const place = await Place.findById(id);
+      if (!place) {
+        return response.status(404).json({ message: "Place not found" });
+      }
+      return response.status(200).json(place);
+    } catch (error) {
+      console.error("Error fetching place:", error);
+      return response.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
+    return response.status(405).json({ message: "Method Not Allowed" });
   }
 }
